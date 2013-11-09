6de5146368ea53f955f8c84fa146c249d864116f
James Yang <james.yang92@gmail.com>
2013-11-08 21:05:28 -0600

    added entry page and two buttons to redirect to catalog page. also added some styling and background image to main page banner

--- ministore_app/load.js
+++ ministore_app/load.js
@@ -1,9 +1,9 @@
 $ = function(selector) { return document.querySelector(selector); }
 
-$('#fullscreen').onclick = function(e) {
+$('#fullscreen-link').onclick = function(e) {
     setTimeout(chrome.app.window.current().fullscreen, 0);
-}
+}   
 
-$('#restore').onclick = function(e) {
+$('#restore-link').onclick = function(e) {
     setTimeout(chrome.app.window.current().restore, 0);
 }
