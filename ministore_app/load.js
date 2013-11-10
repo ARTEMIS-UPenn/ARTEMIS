7146b9a0be9b18c1009b7e8b0b802a010656fb04
James Yang <james.yang92@gmail.com>
2013-11-09 20:03:12 -0600

    finished main page layout and started with inventory and toolcheckout pages

--- ministore_app/load.js
+++ ministore_app/load.js
@@ -7,3 +7,29 @@ $('#fullscreen-link').onclick = function(e) {
 $('#restore-link').onclick = function(e) {
     setTimeout(chrome.app.window.current().restore, 0);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    startTime();
+});
+
+function startTime() {
+    var today=new Date();
+    
+    var day=today.getDate();
+    var month=today.getMonth()+1;
+    var year=today.getFullYear();
+
+    var hour=today.getHours();
+    var minute=today.getMinutes();
+    var second=today.getSeconds();
+    minute=checkTime(minute);
+    second=checkTime(second);
+    document.getElementById('time').innerHTML=month+"/"+day+"/"+year + '&nbsp' +hour+":"+minute+":"+second;
+    t=setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i < 10)
+	i="0" + i;
+    return i;
+}
