7ea9d7b0da457ab01351b33c73d6da0def38e4e4
James Yang <james.yang92@gmail.com>
2014-03-14 16:02:03 -0400

    last commit before wiping Ubuntu on computer

--- Atmel/Atmega328p-pu/main.c
+++ Atmel/Atmega328p-pu/main.c
@@ -4,15 +4,51 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <math.h>
+#include <util/delay.h>
 
 // avr library
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+#include <avr/eeprom.h>
 
 // common libraries
 #include "uart.h"
 #include "DataPacket.h"
+
+
+// micro device id
+// to be moved to eeprom
+#define MMC_ID 0x04
+
+// packet instruction types
+enum {
+  READ_CMD = 0x01,
+  WRITE_CMD
+};
+
+// write instructions
+enum {
+  DISPENSE = 0x01,
+  RETRACT,
+  UNLOCK,
+  ID,
+  LED_ON,
+  LED_OFF
+};
+
+// read instructions
+enum {
+  STATUS = 0x01,
+  ITEM_COUNT
+};
+  
+// compartment status
+
+enum {
+  OK = 0x00,
+  JAM
+  
+};
 
 ISR(TIMER1_OVF_vect) {}
 
@@ -54,16 +90,53 @@ int main(void) {
   PacketInit(&host_packet);
   int len = 0;
   char c;
- 
-  uint8_t message_buf[] = {0xFE, 0xFE, 0x01, 0x04, 0x02, 0x2B, 0x01, 0xCC};
-  int i;
   while(1) {
     c = uart0_getchar();
     if (c != EOF) {
       len = PacketProcessChar(&host_packet, c);
     }
     if (len > 0) {
-      PORTD ^= (1 << PD5);
+      if (PacketGetId(&host_packet) == MMC_ID) { // if i'm being called
+	// check what they want from me
+	switch (PacketGetType(&host_packet)) {
+	case READ_CMD:
+	  break;
+	case WRITE_CMD:
+	  int pl_size = PacketGetPayloadSize(&host_packet);
+	  uint8_t* pl = PacketGetData(&host_packet);
+	  int i;
+	  for (i = 0; i < pl_size; i++) {
+	    switch(pl[i]) {
+	    case DISPENSE:
+	      servoTurn(180);
+	      break;
+	    case RETRACT:
+	      servoTurn(0);
+	      break;
+	    case UNLOCK:
+	      //TODO
+	      break;
+	    case ID:
+	      //TODO
+	      break;
+	    case LED_ON:
+	      //TODO
+	      break;
+	    case LED_OFF:
+	      //TODO
+	      break;
+	    default:
+	      break;
+	    }
+	  }
+	  
+	  servoTurn(180);
+	  break;
+	default:
+	  break;
+	}
+      }
+      //PORTD ^= (1 << PD5);
       len = 0;
     }
   }
