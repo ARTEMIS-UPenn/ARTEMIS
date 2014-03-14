4a51852c8c8733fed59208f54e7ac0432def9562
James Yang <james.yang92@gmail.com>
2014-03-14 17:42:19 -0400

    script to install all dev packages

---
+++ package_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt-get install avr-libc avrdude libusb-dev flex byacc bison build-essential screen
+sudo add-apt-repository ppa:kivy-team/kivy
+sudo apt-get update
+sudo apt-get install kivy
