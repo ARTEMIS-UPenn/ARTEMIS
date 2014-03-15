3fc678754f0e10e0acf2485267d8869e5e7b732d
James Yang <james.yang92@gmail.com>
2014-03-15 18:45:12 -0400

    all necessary packages updated

--- package_install.sh
+++ package_install.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-sudo apt-get install avr-libc avrdude libusb-dev flex byacc bison build-essential screen
-sudo add-apt-repository ppa:kivy-team/kivy
-sudo apt-get update
-sudo apt-get install kivy
+sudo apt-get install git git-core openssh-client openssh-server emacs vim avr-libc avrdude libusb-dev flex byacc bison build-essential
