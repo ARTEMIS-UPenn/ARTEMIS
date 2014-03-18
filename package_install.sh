d352a968f9ed29cfcd1583f6f957cacc50f37105
James Yang <james.yang92@gmail.com>
2014-03-18 14:35:59 -0400

    added LAMP stack install and kivy install

--- package_install.sh
+++ package_install.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-sudo apt-get install git git-core openssh-client openssh-server emacs vim avr-libc avrdude libusb-dev flex byacc bison build-essential
+
+# install kivy
+sudo add-apt-repository ppa:kivy-team/kivy
+sudo apt-get update
+sudo apt-get install kivy
+
+# install dev environment. not necessary if you're putting the code into
+# the final product
+#sudo apt-get install git git-core openssh-client openssh-server emacs vim avr-libc avrdude libusb-dev flex byacc bison build-essential
+
+# install the LAMP stack
+sudo apt-get install apache2 mysql-server php5
